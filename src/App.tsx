2fdbaa32ee987c64649b0bd89e4afb7f8e9633d3
AB Siddik <siddik.prgmr@gmail.com>
2023-05-26 18:26:54 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import HangmanDrawing from './components/HangmanDrawing';
+import HangmanWord from './components/HangmanWord';
+import Keyboard from './components/Keyboard';
+import words from './wordList.json';
+function App() {
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center",
+    }}>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,13 @@
+
+const HangmanDrawing = () => {
+    return (
+        <div style={{ position: "relative" }}>
+            <div style={{ height: "50px", width: "10px", background: "#000000",position: "absolute", top: 0, right: 0 }} />
+            <div style={{ height: "10px", width: "150px", background: "#000000", marginLeft: "120px" }} />
+            <div style={{ height: "400px", width: "10px", background: "#000000", marginLeft: "120px" }} />
+            <div style={{ height: "10px", width: "250px", background: "#000000" }} />
+        </div>
+    )
+}
+
+export default HangmanDrawing
